52bfae3617177651f768a92ac6d8a62818797479
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-02-11 01:13:22 +0530

    Create Hello World Newline.c

---
+++ Day 1/Practice/Hello World Newline.c
@@ -0,0 +1,14 @@
+// Hello World with a new line
+// Write a program to print the given sample output.
+
+// Output (stdout):
+// Hello World
+// Hello World
+
+#include <stdio.h>
+int main()
+{
+   printf("Hello World\n");
+   printf("Hello World");
+   return 0;
+}
