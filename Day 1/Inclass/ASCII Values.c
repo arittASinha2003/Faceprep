5c8ca21875ed6307efd8761ca16bc48bbfe221fd
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2024-02-10 20:16:30 +0530

    Create ASCII Values.c

---
+++ Day 1/Inclass/ASCII Values.c
@@ -0,0 +1,22 @@
+// ASCII Values - I
+
+// Write a program to get a character as input and print its ASCII value.
+
+// Sample Input 1:
+// a
+// Sample Output 1:
+// 97
+
+// Sample Input 2:
+// A
+// Sample Output 2:
+// 65
+
+#include <stdio.h>
+int main()
+{
+  char a;
+  scanf("%c", &a);
+  printf("%d", a);
+  return 0;
+}
